d9b78937a709ca3927b4913b4e66acf81c22bf46
xiuri <jmbregalado@gmail.com>
2021-05-09 23:08:01 +0800

    update clickable icons

--- cifirproject/cifir/urls.py
+++ cifirproject/cifir/urls.py
@@ -9,8 +9,18 @@ urlpatterns = [
     # path('api/data', views.get_data, name='api-data'),
 
     #TEST URL
-    path('index/', views.indexView.as_view(), name="cifir_index_view"),
-    path('home/', views.homePageView.as_view(), name="cifir_home_view"),
+    path('index/', views.indexView.as_view(), name="index_view"),
+
+    path('home/', views.homePageView.as_view(), name="home_view"),
+    path('collections/', views.collectionsPageView.as_view(), name="collections_view"),
+    path('bookmarks/', views.bookmarksPageView.as_view(), name="bookmarks_view"),
+    path('audiobooks/', views.audiobooksPageView.as_view(), name="audiobooks_view"),
+    path('networklibraries/', views.networkLibrariesPageView.as_view(), name="networklibraries_view"),
+
+    path('favorites/', views.favoritesPageView.as_view(), name="favorites_view"),
+    path('toread/', views.toReadPageView.as_view(), name="toread_view"),
+    path('haveread/', views.haveReadPageView.as_view(), name="haveread_view"),
+    
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cifirproject/cifir/views.py
+++ cifirproject/cifir/views.py
@@ -10,3 +10,31 @@ class indexView(View):
 class homePageView(View):
 	def get(self, request):
 		return render(request,'homepage.html')
+
+class audiobooksPageView(View):
+	def get(self, request):
+		return render(request,'audiobooks.html')
+
+class bookmarksPageView(View):
+	def get(self, request):
+		return render(request,'bookmarks.html')
+
+class collectionsPageView(View):
+	def get(self, request):
+		return render(request,'collections.html')
+
+class favoritesPageView(View):
+	def get(self, request):
+		return render(request,'favorites.html')
+
+class haveReadPageView(View):
+	def get(self, request):
+		return render(request,'haveRead.html')
+
+class toReadPageView(View):
+	def get(self, request):
+		return render(request,'toRead.html')
+
+class networkLibrariesPageView(View):
+	def get(self, request):
+		return render(request,'networklibraries.html')
