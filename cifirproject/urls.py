64863ec1d41097a919d000fcc010f3def4619ddf
Diane Isabelle Lambojo <72972840+dianelambojo@users.noreply.github.com>
2022-02-06 21:41:18 +0800

    Revert "Update urls.py"
    This reverts commit b038694a1bc549fd229a043b4bda89af95e57472.

--- cifirproject/urls.py
+++ cifirproject/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('cifir.urls', namespace='cifir'))
+    path('cifir/', include('cifir.urls', namespace='cifir'))
 
 ]
 
